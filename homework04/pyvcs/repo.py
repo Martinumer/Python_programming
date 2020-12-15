e12a55fba4ae96ff522870047d405a868ece926a
Martinumer <maria.armstrong228@gmail.com>
2020-12-15 21:47:43 +0300

    fixed repo

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -4,53 +4,40 @@ import typing as tp
 
 
 def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    name = os.environ.get("GIT_DIR", ".git")
-    workdir = pathlib.Path(workdir).absolute()
-    next_dir = workdir
-    flag = False
-    while next_dir != os.path.abspath(".").split(os.path.sep)[0] + os.path.sep:
-        ls = os.listdir(next_dir)
-        if name in ls:
-            flag = True
-            way = str(next_dir) + os.path.sep + name
-            return pathlib.Path(way).absolute()
-        next_dir = os.path.split(next_dir)[0]
-    if not flag:
-        raise Exception0("Not a git repository")
-
-
-class Exception0(Exception):
-    def __init__(self, text):
-        self.txt = text
+    gitdir_name = os.environ["GIT_DIR"] if "GIT_DIR" in os.environ else ".pyvcs"
+    workdir = pathlib.Path(workdir)
+    while str(workdir.absolute()) != "/":
+        if (workdir / gitdir_name).is_dir():
+            return workdir / gitdir_name
+        workdir = workdir.parent
+    if (workdir / gitdir_name).is_dir():
+        return workdir / gitdir_name
+    raise Exception("Not a git repository")
 
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
+    gitdir_name = os.environ["GIT_DIR"] if "GIT_DIR" in os.environ else ".pyvcs"
     workdir = pathlib.Path(workdir)
-    if os.path.isfile(workdir):
-        raise Exception0(f"{workdir} is not a directory")
-    elif not os.path.exists(workdir):
-        raise Exception0("Not a git repository")
-    name = os.environ.get("GIT_DIR", ".git")
-    gitdir = workdir / name
-    gitdir.mkdir(parents=True)
-    nextdir = gitdir / "HEAD"
-    f = open(nextdir, "w")
-    f.write("ref: refs/heads/master\n")
-    f.close()
-    nextdir = gitdir / "config"
-    f = open(nextdir, "w")
-    f.write(
-        "[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = false\n"
-    )
-    f.close()
-    nextdir = gitdir / "description"
-    f = open(nextdir, "w")
-    f.write("Unnamed pyvcs repository.\n")
-    f.close()
-    nextdir = gitdir / "objects"
-    nextdir.mkdir()
-    nextdir = gitdir / "refs" / "heads"
-    nextdir.mkdir(parents=True)
-    nextdir = gitdir / "refs" / "tags"
-    nextdir.mkdir(parents=True)
-    return gitdir
+    if workdir.is_file():
+        raise Exception(f"{workdir} is not a directory")
+    (workdir / gitdir_name).mkdir()
+    HEAD = "HEAD"
+    with open(workdir / gitdir_name / HEAD, "w") as f:
+        f.write("ref: refs/heads/master\n")
+    config = "config"
+    with open(workdir / gitdir_name / config, "w") as f:
+        f.write(
+            "[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = false\n"
+        )
+    description = "description"
+    with open(workdir / gitdir_name / description, "w") as f:
+        f.write("Unnamed pyvcs repository.\n")
+    objects = "objects"
+    (workdir / gitdir_name / objects).mkdir()
+    refs = "refs"
+    (workdir / gitdir_name / refs).mkdir()
+    heads = "heads"
+    (workdir / gitdir_name / refs / heads).mkdir()
+    tags = "tags"
+    (workdir / gitdir_name / refs / tags).mkdir()
+    return workdir / gitdir_name
