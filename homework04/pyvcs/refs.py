19d86631bb35c94da6a00ccb95b8a778fe763ab3
Martinumer <maria.armstrong228@gmail.com>
2020-12-16 12:52:21 +0300

    ref

--- homework04/pyvcs/refs.py
+++ homework04/pyvcs/refs.py
@@ -8,8 +8,9 @@ def update_ref(gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value
 
 
 def symbolic_ref(gitdir: pathlib.Path, name: str, ref: str) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    ref_file = gitdir / name
+    with (ref_file).open("w") as file:
+        file.write(ref)
 
 
 def ref_resolve(gitdir: pathlib.Path, refname: str) -> tp.Optional[str]:
