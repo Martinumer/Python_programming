730c4dbb24882d1072bb9678094c65bd5902bb99
Martinumer <maria.armstrong228@gmail.com>
2021-01-03 22:19:58 +0300

    init isort

--- homework05/vkapi/__init__.py
+++ homework05/vkapi/__init__.py
@@ -1,4 +1,4 @@
-from vkapi import config #type: ignore
-from vkapi.session import Session #type: ignore
+from vkapi import config  # type: ignore
+from vkapi.session import Session  # type: ignore
 
 session = Session(config.VK_CONFIG["domain"])
