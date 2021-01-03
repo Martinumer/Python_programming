2cce9af0dd6acdc679fda21da93477b846ba9acf
Martinumer <maria.armstrong228@gmail.com>
2021-01-04 00:58:47 +0300

    wall

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -22,30 +22,42 @@ def get_posts_2500(
     fields: tp.Optional[tp.List[str]] = None,
 ) -> tp.Dict[str, tp.Any]:
     script = f"""
-                var i = 0;
+                var i = 0; 
                 var result = [];
-                while i < {max_count} {{
-                    result.push(
-                                API.wall.get(
-                                            {
-                                            {
-                                            f"owner_id: {owner_id}",
-                                            f"domain: {domain}",
-                                            f"offset: {offset} + i",
-                                            f"count: {count}",
-                                            f"filter: {filter}",
-                                            f"extended: {extended}",
-                                            f"fields: {fields}"    
-                                            }
-                                            }
-                                            )
-                                )
-                    i = i + {count}
-                }}return ;
+                while (i < {max_count}){{
+                    if ({offset}+i+100 > {count}){{
+                        result.push(API.wall.get({{
+                            "owner_id": "{owner_id}",
+                            "domain": "{domain}",
+                            "offset": "{offset} +i",
+                            "count": "{count}-(i+{offset})",
+                            "filter": "{filter}",
+                            "extended": "{extended}",
+                            "fields": "{fields}"
+                        }}));
+                    }} 
+                    result.push(API.wall.get({{
+                        "owner_id": "{owner_id}",
+                        "domain": "{domain}",
+                        "offset": "{offset} +i",
+                        "count": "{count}",
+                        "filter": "{filter}",
+                        "extended": "{extended}",
+                        "fields": "{fields}"
+                    }}));
+                    i = i + {max_count};
+                }}
+                return result;
             """
-    data = {"code": script}
-    response = session.post("/execute", data=data).json()["response"]
-    return response["items"]
+    data = {
+        "code": script,
+        "access_token": VK_CONFIG["access_token"],
+        "v": VK_CONFIG["version"],
+    }
+    response = session.post("execute", data=data)
+    if "error" in response.json() or not response.ok:
+        raise APIError(response.json()["error"]["error_msg"])
+    return response.json()["response"]["items"]
 
 
 def get_wall_execute(
@@ -74,31 +86,37 @@ def get_wall_execute(
     :param fields: Список дополнительных полей для профилей и сообществ, которые необходимо вернуть.
     :param progress: Callback для отображения прогресса.
     """
-    old_data = pd.DataFrame()
-    code = f"""
-            return API.wall.get ({{
-            "owner_id": "{owner_id}",
-            "domain": "{domain}",
-            "offset": "0",
-            "count": "1",
-            "filter": "{filter}",
-            "extended": "0",
-            "fields": ""
-}});
-"""
-    data = {"code": code}
-    response = session.post("/execute", data=data).json()
-    if "error" in response:
-        raise APIError(response["error"]["error_msg"])
+    df = []
+    response = session.post(
+        "execute",
+        data={
+            "code": f'return {{"count": API.wall.get({{"owner_id": "{owner_id}","domain": "{domain}","offset": "0","count": "1","filter": "{filter}"}}).count}};',
+            "access_token": VK_CONFIG["access_token"],
+            "v": VK_CONFIG["version"],
+        },
+    )
+    if "error" in response.json() or not response.ok:
+        raise APIError(response.json()["error"]["error_msg"])
+    if count != 0:
+        count = min(count, response.json()["response"]["count"])
+    else:
+        count = response.json()["response"]["count"]
     if progress is None:
         progress = lambda x: x
-    for _ in progress(
-        range(0, math.ceil((response["response"]["count"] if count == 0 else count) / max_count))
-    ):
-        old_data = old_data.append(
-            json_normalize(
-                get_posts_2500(owner_id, domain, offset, count, max_count, filter, extended, fields)
+
+    for i in progress(range((count + max_count - 1) // max_count)):
+        df.append(
+            get_posts_2500(
+                owner_id,
+                domain,
+                offset + i * max_count,
+                count,
+                max_count,
+                filter,
+                extended,
+                fields,
             )
         )
-        time.sleep(1)
-    return old_data
+        if i % 3 == 1:
+            time.sleep(1)
+    return json_normalize(df)
