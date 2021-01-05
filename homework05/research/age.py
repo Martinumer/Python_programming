81f1e4b44ce0d5445f26eb5eb61540f9050dae0a
Martinumer <maria.armstrong228@gmail.com>
2021-01-05 23:33:21 +0300

    age

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -1,11 +1,19 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=unused-argument
+# pylint: disable=unused-import
+# pylint: disable=too-many-arguments
+# pylint: disable=redefined-builtin
+
 import datetime as dt
 import statistics
 import typing as tp
 
-from vkapi.friends import get_friends  # type: ignore
+from vkapi.friends import get_friends
 
 
-def age_predict(user_id=320832582) -> tp.Optional[float]:
+def age_predict(user_id: int) -> tp.Optional[float]:
     """
     Наивный прогноз возраста пользователя по возрасту его друзей.
 
@@ -14,20 +22,15 @@ def age_predict(user_id=320832582) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    age = []
-    this = dt.date.today()
     friends = get_friends(user_id, fields=["bdate"]).items
+    years_of_birth = []
     for friend in friends:
         try:
             bdate = dt.datetime.strptime(friend["bdate"], "%d.%m.%Y")  # type: ignore
         except (KeyError, ValueError):
             continue
-        age.append(
-            this.year
-            - bdate.year
-            - (this.month < bdate.month or (this.month == bdate.month and this.day < bdate.day))
-        )
-
-    if age:
-        return statistics.median(age)
+        years_of_birth.append(dt.datetime.now().year - bdate.year)
+    if years_of_birth:
+        return float(statistics.median(years_of_birth))
     return None
+
