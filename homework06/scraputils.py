39abab8d8db4e14c47349fc5b3f140b9bb035466
Martinumer <maria.armstrong228@gmail.com>
2021-03-23 20:13:27 +0300

    scr black

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -4,11 +4,13 @@ from bs4 import BeautifulSoup
 
 def extract_news(parser):
     """ Extract news from a given web page """
+
     def extract_first_integer_from_tag(tag, separator):
         try:
             return 0 if tag is None else int(tag.text.split(separator)[0])
         except ValueError:
             return 0
+
     news_list = []
 
     news = parser.findAll("a", {"class": "storylink"})
@@ -16,24 +18,25 @@ def extract_news(parser):
 
     for new in range(len(news)):
         author = subtexts[new].find("a", {"class": "hnuser"})
-        comments = extract_first_integer_from_tag(subtexts[new].find_all("a")[-1], '\xa0')
-        points = extract_first_integer_from_tag(subtexts[new].find("span", {"class": "score"}), ' ')
+        comments = extract_first_integer_from_tag(subtexts[new].find_all("a")[-1], "\xa0")
+        points = extract_first_integer_from_tag(subtexts[new].find("span", {"class": "score"}), " ")
 
-        news.append({
-            'author': None if author is None else author.text,
-            'comments': comments,
-            'points': points,
-            'title': news[new].text,
-            'url': news[new]['href']
-        })
+        news.append(
+            {
+                "author": None if author is None else author.text,
+                "comments": comments,
+                "points": points,
+                "title": news[new].text,
+                "url": news[new]["href"],
+            }
+        )
 
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    return parser.find('a', {"class": "morelink"})['href']
-
+    return parser.find("a", {"class": "morelink"})["href"]
 
 
 def get_news(url, n_pages=1):
@@ -49,4 +52,3 @@ def get_news(url, n_pages=1):
         news.extend(news_list)
         n_pages -= 1
     return news
-
