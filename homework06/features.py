61eefa32e7b93feabb765bdd7b8da6d0b333c94e
Martinumer <maria.armstrong228@gmail.com>
2021-04-06 22:51:12 +0300

    features

--- homework06/features.py
+++ homework06/features.py
@@ -6,8 +6,8 @@ model = NaiveBayesClassifier(alpha=0.05)
 s = session()
 rows = s.query(News).filter(News.label != None).all()
 for row in rows:
-	X.append(f"{row.title} {row.author} {row.url}")
-	y.append(row.label)
+    X.append(f"{row.title} {row.author} {row.url}")
+    y.append(row.label)
 X_train, y_train, X_test, y_test = X[:735], y[:735], X[735:], y[735:]
 model.fit(X, y)
 print(model.score(X_test, y_test))
