92a9ceada9fc261933b1e30093e33b8c3aeee8a6
Martinumer <maria.armstrong228@gmail.com>
2021-05-07 13:59:40 +0300

    ft

--- homework06/features.py
+++ homework06/features.py
@@ -9,5 +9,5 @@ for row in rows:
     X.append(f"{row.title} {row.author} {row.url}")
     y.append(row.label)
 X_train, y_train, X_test, y_test = X[:735], y[:735], X[735:], y[735:]
-model.fit(X, y)
+model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
